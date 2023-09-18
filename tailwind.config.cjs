5df07676ded303cc5d67a81ade5a57359ef2ecfa
jezsdaniel <jadendaniel7@gmail.com>
2023-09-18 17:15:39 -0400

    Add header info

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'light-green': '#00FFC2',
+        'dark-green': '#061B0B',
+        'slate-blue': '#4E8098',
+        yellow: '#FFFC31',
+        red: '#F2545B',
+      },
+      fontFamily: {
+        sans: ['Rubik Variable', ...defaultTheme.fontFamily.sans],
+        body: ['Inter Variable', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
